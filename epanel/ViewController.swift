31b6dfe43282363c6fce46b0923f4fb875568291
Al <al_biheiri@apple.com>
2022-11-17 16:36:04 -0800

    Initial Commit

---
+++ epanel/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  epanel
+//
+//  Created by Al on 11/17/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
